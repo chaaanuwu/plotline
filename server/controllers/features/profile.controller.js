031e7cb5abc8fa921e141fa456980b5440c5e6ab
chaaanuwu <gkchanuwa@gmail.com>
2026-03-05 01:20:29 +0530

    Add profile feature

---
+++ server/controllers/features/profile.controller.js
@@ -0,0 +1,30 @@
+import { getUserMeData } from "../user.controller.js";
+import { getFollowersData, getFollowingData } from "../user_follows.controller.js";
+
+export const getUserMeProfile = async (req, res) => {
+    try {
+        const userId = req.user.userId;
+
+        const [user, followers, following] = await Promise.all([
+            getUserMeData(userId),
+            getFollowersData(userId),
+            getFollowingData(userId)
+        ]);
+
+        res.status(200).json({
+            success: true,
+            user,
+            followersCount: followers.length,
+            followers,
+            followingCount: following.length,
+            following
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            error: "Server error"
+        });
+    }
+};
