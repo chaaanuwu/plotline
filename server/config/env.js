e63e85670586b401c676459c1a005e2cd487146d
chaaanuwu <gkchanuwa@gmail.com>
2026-06-04 21:57:34 +0530

    fix: move dotenv config call inside production check

--- server/config/env.js
+++ server/config/env.js
@@ -1,6 +1,10 @@
 import { config } from 'dotenv';
+
 const env = process.env.NODE_ENV || "development";
-config({ path: `.env.${env}.local` });
+
+if (process.env.NODE_ENV !== "production") {
+    config({ path: `.env.${env}.local` });
+}
 
 export const {
     PORT,
